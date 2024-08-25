5e53769ec069a54e35aed402d83e7cee92923493
ghili <ghiliaweld@gmail.com>
2024-08-24 22:29:36 -0400

    beacon state benches

--- benches/arena.rs
+++ benches/arena.rs
@@ -1,17 +1,25 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use grandine_ssz::SszRead;
 use grandine_types::{
-    combined::SignedBeaconBlock as GrandineBeaconBlock, config::Config, preset::Mainnet,
+    combined::{BeaconState as GrandineBeaconState, SignedBeaconBlock as GrandineBeaconBlock},
+    config::Config,
+    preset::Mainnet,
 };
-use sigp_types::{ForkName, MainnetEthSpec, SignedBeaconBlock as SigpBeaconBlock};
+use sigp_types::{
+    BeaconState as SigpBeaconState, ChainSpec, ForkName, MainnetEthSpec,
+    SignedBeaconBlock as SigpBeaconBlock,
+};
 
 fn ssz_arena(c: &mut Criterion) {
     let mut group = c.benchmark_group("SSZ Decode");
+
     let block_bytes: Vec<u8> = std::fs::read("beacon-block.ssz").unwrap();
+    let state_bytes: Vec<u8> = std::fs::read("beacon-state.ssz").unwrap();
+
     for bytes in [block_bytes].iter() {
         #[cfg(feature = "sigp")]
         group.bench_with_input(
-            BenchmarkId::new("Lighthouse", "SignedBeaconBlock decode"),
+            BenchmarkId::new("Lighthouse", "SignedBeaconBlock"),
             bytes,
             |b, bytes| {
                 b.iter(|| {
@@ -24,7 +32,7 @@ fn ssz_arena(c: &mut Criterion) {
         );
         #[cfg(feature = "grandine")]
         group.bench_with_input(
-            BenchmarkId::new("Grandine", "SignedBeaconBlock decode"),
+            BenchmarkId::new("Grandine", "SignedBeaconBlock"),
             bytes,
             |b, bytes| {
                 b.iter(|| {
@@ -33,6 +41,30 @@ fn ssz_arena(c: &mut Criterion) {
             },
         );
     }
+
+    for bytes in [state_bytes].iter() {
+        #[cfg(feature = "sigp")]
+        group.bench_with_input(
+            BenchmarkId::new("Lighthouse", "BeaconState"),
+            bytes,
+            |b, bytes| {
+                b.iter(|| {
+                    SigpBeaconState::<MainnetEthSpec>::from_ssz_bytes(bytes, &ChainSpec::default())
+                })
+            },
+        );
+        #[cfg(feature = "grandine")]
+        group.bench_with_input(
+            BenchmarkId::new("Grandine", "BeaconState"),
+            bytes,
+            |b, bytes| {
+                b.iter(|| {
+                    GrandineBeaconState::<Mainnet>::from_ssz_unchecked(&Config::mainnet(), bytes)
+                })
+            },
+        );
+    }
+
     group.finish();
 }
 
