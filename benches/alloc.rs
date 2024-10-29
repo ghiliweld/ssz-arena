32ca586f30d4e9275e03a264c8ad509adb8d51a0
ghili <ghiliaweld@gmail.com>
2024-10-28 22:51:00 -0400

    added sszb alloc benches

--- benches/alloc.rs
+++ benches/alloc.rs
@@ -1,18 +1,97 @@
 use milhouse::List;
+#[cfg(all(feature = "sigp", feature = "block"))]
+use sigp_types::{
+    ssz_tagged_signed_beacon_block::encode::as_ssz_bytes as sigp_block_encode, ForkName,
+    MainnetEthSpec, SignedBeaconBlock as SigpBeaconBlock,
+};
 #[cfg(all(feature = "sigp", feature = "state"))]
 use sigp_types::{BeaconState as SigpBeaconState, ChainSpec, MainnetEthSpec};
-#[cfg(all(feature = "sigp", feature = "block"))]
-use sigp_types::{ForkName, MainnetEthSpec, SignedBeaconBlock as SigpBeaconBlock};
+#[cfg(feature = "sigp")]
 use ssz::{Decode, Encode};
 #[cfg(feature = "block")]
 use ssz_arena::get_block_bytes;
 #[cfg(feature = "state")]
 use ssz_arena::get_state_bytes;
+#[cfg(all(feature = "sszb", feature = "block"))]
+use ssz_arena::SignedBeaconBlock;
+#[cfg(feature = "sszb")]
+use sszb::{SszDecode, SszEncode};
 
 #[global_allocator]
 static ALLOC: dhat::Alloc = dhat::Alloc;
 
-fn milhouse_decode_profiling() {
+#[cfg(feature = "sszb")]
+fn sszb_list_decode_profiling() {
+    type C = typenum::U1099511627776;
+    const N: u64 = 1_000_000;
+
+    // basic test case
+    let size = N;
+    let list = List::<u64, C>::try_from_iter(0..size).unwrap();
+    let list_bytes = list.to_ssz();
+
+    let _profiler = dhat::Profiler::builder()
+        .testing()
+        .trim_backtraces(None)
+        .build();
+
+    let start_stats = dhat::HeapStats::get();
+
+    // measure decoding
+    let _list = <List<u64, C> as SszDecode>::from_ssz_bytes(list_bytes.as_slice()).unwrap();
+
+    // print decoding stats
+    let end_stats = dhat::HeapStats::get();
+
+    println!("== sszb: milhouse list decoding");
+    println!(
+        "   ├─ allocated {} bytes",
+        end_stats.curr_bytes - start_stats.curr_bytes
+    );
+    println!(
+        "   ╰─ allocated {} times",
+        end_stats.curr_blocks - start_stats.curr_blocks
+    );
+}
+
+#[cfg(feature = "sszb")]
+fn sszb_list_encode_profiling() {
+    type C = typenum::U1099511627776;
+    const N: u64 = 1_000_000;
+
+    // basic test case
+    let size = N;
+    let list = List::<u64, C>::try_from_iter(0..size).unwrap();
+
+    let len = SszEncode::ssz_bytes_len(&list);
+    let mut buf: Vec<u8> = vec![0u8; len];
+
+    let _profiler = dhat::Profiler::builder()
+        .testing()
+        .trim_backtraces(None)
+        .build();
+
+    let start_stats = dhat::HeapStats::get();
+
+    // measure encoding
+    let _list_bytes = list.ssz_write(&mut buf.as_mut_slice());
+
+    // print encoding stats
+    let end_stats = dhat::HeapStats::get();
+
+    println!("== sszb: milhouse list encoding");
+    println!(
+        "   ├─ allocated {} bytes",
+        end_stats.curr_bytes - start_stats.curr_bytes
+    );
+    println!(
+        "   ╰─ allocated {} times",
+        end_stats.curr_blocks - start_stats.curr_blocks
+    );
+}
+
+#[cfg(feature = "sigp")]
+fn sigp_list_decode_profiling() {
     type C = typenum::U1099511627776;
     const N: u64 = 1_000_000;
 
@@ -34,7 +113,7 @@ fn milhouse_decode_profiling() {
     // print decoding stats
     let end_stats = dhat::HeapStats::get();
 
-    println!("== milhouse list decoding");
+    println!("== sigp: milhouse list decoding");
     println!(
         "   ├─ allocated {} bytes",
         end_stats.curr_bytes - start_stats.curr_bytes
@@ -45,7 +124,8 @@ fn milhouse_decode_profiling() {
     );
 }
 
-fn milhouse_encode_profiling() {
+#[cfg(feature = "sigp")]
+fn sigp_list_encode_profiling() {
     type C = typenum::U1099511627776;
     const N: u64 = 1_000_000;
 
@@ -66,7 +146,71 @@ fn milhouse_encode_profiling() {
     // print encoding stats
     let end_stats = dhat::HeapStats::get();
 
-    println!("== milhouse list encoding");
+    println!("== sigp: milhouse list encoding");
+    println!(
+        "   ├─ allocated {} bytes",
+        end_stats.curr_bytes - start_stats.curr_bytes
+    );
+    println!(
+        "   ╰─ allocated {} times",
+        end_stats.curr_blocks - start_stats.curr_blocks
+    );
+}
+
+#[cfg(all(feature = "sszb", feature = "block"))]
+fn sszb_block_decode_profiling() {
+    let beacon_block_bytes: Vec<u8> =
+        std::fs::read("beacon-block.ssz").unwrap_or(get_block_bytes().unwrap());
+
+    let _profiler = dhat::Profiler::builder()
+        .testing()
+        .trim_backtraces(None)
+        .build();
+
+    let start_stats = dhat::HeapStats::get();
+
+    // measure decoding
+    let _beacon_block =
+        <SignedBeaconBlock as SszDecode>::from_ssz_bytes(beacon_block_bytes.as_slice()).unwrap();
+
+    // print decoding stats
+    let end_stats = dhat::HeapStats::get();
+
+    println!("== sszb: beacon block decoding");
+    println!(
+        "   ├─ allocated {} bytes",
+        end_stats.curr_bytes - start_stats.curr_bytes
+    );
+    println!(
+        "   ╰─ allocated {} times",
+        end_stats.curr_blocks - start_stats.curr_blocks
+    );
+}
+
+#[cfg(all(feature = "sszb", feature = "block"))]
+fn sszb_block_encode_profiling() {
+    let beacon_block_bytes: Vec<u8> =
+        std::fs::read("beacon-block.ssz").unwrap_or(get_block_bytes().unwrap());
+    let beacon_block =
+        <SignedBeaconBlock as SszDecode>::from_ssz_bytes(beacon_block_bytes.as_slice()).unwrap();
+
+    let len = SszEncode::ssz_bytes_len(&beacon_block);
+    let mut buf: Vec<u8> = vec![0u8; len];
+
+    let _profiler = dhat::Profiler::builder()
+        .testing()
+        .trim_backtraces(None)
+        .build();
+
+    let start_stats = dhat::HeapStats::get();
+
+    // measure encoding
+    let _block_bytes = beacon_block.ssz_write(&mut buf.as_mut_slice());
+
+    // print encoding stats
+    let end_stats = dhat::HeapStats::get();
+
+    println!("== sszb: beacon block encoding");
     println!(
         "   ├─ allocated {} bytes",
         end_stats.curr_bytes - start_stats.curr_bytes
@@ -99,7 +243,7 @@ fn sigp_block_decode_profiling() {
     // print decoding stats
     let end_stats = dhat::HeapStats::get();
 
-    println!("== beacon block decoding");
+    println!("== sigp: beacon block decoding");
     println!(
         "   ├─ allocated {} bytes",
         end_stats.curr_bytes - start_stats.curr_bytes
@@ -128,12 +272,12 @@ fn sigp_block_encode_profiling() {
     let start_stats = dhat::HeapStats::get();
 
     // measure encoding
-    //let bytes = beacon_block.as_ssz_bytes();
+    let _bytes = sigp_block_encode(&beacon_block);
 
     // print encoding stats
     let end_stats = dhat::HeapStats::get();
 
-    println!("== beacon block encoding");
+    println!("== sigp: beacon block encoding");
     println!(
         "   ├─ allocated {} bytes",
         end_stats.curr_bytes - start_stats.curr_bytes
@@ -145,8 +289,23 @@ fn sigp_block_encode_profiling() {
 }
 
 fn main() {
-    milhouse_decode_profiling();
-    milhouse_encode_profiling();
+    // List alloc benches
+    #[cfg(feature = "sszb")]
+    sszb_list_encode_profiling();
+    #[cfg(feature = "sszb")]
+    sszb_list_decode_profiling();
+
+    #[cfg(feature = "sigp")]
+    sigp_list_encode_profiling();
+    #[cfg(feature = "sigp")]
+    sigp_list_decode_profiling();
+
+    // SignedBeaconBlock alloc benches
+    #[cfg(all(feature = "sszb", feature = "block"))]
+    sszb_block_decode_profiling();
+    #[cfg(all(feature = "sszb", feature = "block"))]
+    sszb_block_encode_profiling();
+
     #[cfg(all(feature = "sigp", feature = "block"))]
     sigp_block_decode_profiling();
     #[cfg(all(feature = "sigp", feature = "block"))]
